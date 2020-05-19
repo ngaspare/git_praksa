dc42b2133e9499822e1407bdfa5faf468bd5736a
Nikola Gašparec <nikola.gasparec@factory.hr>
2020-05-19 15:10:43 +0200

    Cetvrti commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -90,7 +90,6 @@
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://blog.laravel.com">Blog</a>
                     <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
             </div>
