a35bda656586dda72636672cd4ef2480735c0a78
Nikola Gašparec <nikola.gasparec@factory.hr>
2020-05-19 14:37:28 +0200

    Drugi commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Git practice
                 </div>
 
                 <div class="links">
