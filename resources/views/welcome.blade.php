81331dd9b7cb509c740285a66412b8cd63be6abd
Nikola Gašparec <nikola.gasparec@factory.hr>
2020-05-25 20:30:05 +0200

    drugi u master

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -86,7 +86,6 @@
 
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
                 </div>
             </div>
         </div>
