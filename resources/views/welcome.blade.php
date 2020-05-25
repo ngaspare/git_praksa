ec4c1b373f2b7344176f4770650b9abc88d1fc24
Nikola Gašparec <nikola.gasparec@factory.hr>
2020-05-25 20:29:26 +0200

    prvi u master

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -87,8 +87,6 @@
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
                     <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
             </div>
         </div>
