72830ea73d799e61476826926a7c4a5cc9c06fdb
Nikola Gašparec <nikola.gasparec@factory.hr>
2020-05-26 10:28:07 +0200

    1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -87,8 +87,7 @@
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
                     <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://laravel.com">News</a>
                 </div>
             </div>
         </div>
